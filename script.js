89c48fdbe0728599258195022b2ec1434e2fb903
Thonkkk <98953728+Thonkkk@users.noreply.github.com>
2023-02-26 15:55:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-function getAssetInfo() {
-  var assetId = document.getElementById("assetId").value;
-  if (assetId !== "") {
-    var url = "https://api.roblox.com/marketplace/productinfo?assetId=" + assetId;
-    fetch(url)
+const form = document.querySelector('form');
+const assetIdInput = document.querySelector('#assetId');
+const getInfoButton = document.querySelector('#getInfo');
+const assetInfoDiv = document.querySelector('#assetInfo');
+
+getInfoButton.addEventListener('click', () => {
+  const assetId = assetIdInput.value;
+  if (assetId) {
+    fetch(`https://api.roblox.com/marketplace/productinfo?assetId=${assetId}`)
       .then(response => response.json())
       .then(data => {
-        var assetInfo = document.getElementById("assetInfo");
-        assetInfo.innerHTML = "<h2>" + data.Name + "</h2>";
-        assetInfo.innerHTML += "<p>Asset ID: " + data.AssetId + "</p>";
-        assetInfo.innerHTML += "<p>Creator: " + data.Creator.Name + "</p>";
-        assetInfo.innerHTML += "<p>Description: " + data.Description + "</p>";
-        assetInfo.innerHTML += "<p>Price: " + data.PriceInRobux + " Robux</p>";
+        assetInfoDiv.innerHTML = `
+          <h2>${data.Name}</h2>
+          <p><strong>Description:</strong> ${data.Description}</p>
+          <p><strong>Price:</strong> ${data.PriceInRobux.toLocaleString()} Robux</p>
+          <p><strong>Is Limited:</strong> ${data.IsLimited ? 'Yes' : 'No'}</p>
+          <p><strong>Remaining:</strong> ${data.Remaining.toLocaleString()}</p>
+          <p><strong>Owner:</strong> ${data.Creator.Name}</p>
+        `;
       })
-      .catch(error => console.error(error));
+      .catch(error => {
+        assetInfoDiv.textContent = 'Error retrieving asset info. Please check the asset ID and try again.';
+        console.error(error);
+      });
+  } else {
+    assetInfoDiv.textContent = 'Please enter an asset ID.';
   }
-}
+});
